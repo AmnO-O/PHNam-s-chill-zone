36f94bdb8e80f89b9fcf18e39d263e4adc55671a
Nam Pham <nam722006@gmail.com>
2025-05-20 16:54:30 +0700

    smooth the slider and blogs

--- blog.js
+++ blog.js
@@ -16,3 +16,92 @@ function blogPopUp(){
 }
 
 blogPopUp(); 
+
+
+const blogs = document.querySelectorAll('.blog-container'); 
+blogs.forEach(initBlog); 
+
+
+function initBlog(blog){
+    const blog_track = blog.querySelector('.blog-track'); 
+    const items = blog_track.querySelectorAll('.blog-post'); 
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+
+    let currentTranslate = 0;
+    let prevTranslate = 0;
+    let currentIndex = 0;
+
+    const itemWidth = items[0].offsetWidth + parseInt(getComputedStyle(items[0]).marginLeft) * 2;
+    blog_track.style.width = `${itemWidth * items.length}px`;
+    
+
+    prevButton.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            centerItem(currentIndex);
+        }
+    });
+
+    nextButton.addEventListener('click', () => {
+        if (currentIndex < items.length - 1) {
+            currentIndex++;
+            centerItem(currentIndex);
+        }
+    });
+
+    setTimeout(() => {
+        if (items.length > 0) {
+            centerItem(0); 
+        }
+    }, 100);
+    
+
+    function centerItem(index) {
+
+        const blogALL =  blog.offsetWidth;
+        const itemCenter = items[index].offsetWidth / 2;
+        const centerOffset = blogALL / 2 - itemCenter;
+        
+        currentIndex = index;
+        currentTranslate = centerOffset - (index * itemWidth);
+        prevTranslate = currentTranslate;
+        
+        setSliderPosition();
+        updateActiveItems(index);
+    }
+
+    function setSliderPosition() {
+        blog_track.style.transform = `translateX(${currentTranslate}px)`;
+    }
+
+    function updateActiveItems(index) {
+
+        items.forEach((item, i) => {
+            item.classList.remove('active');            
+        });
+        
+        items[index].classList.add('active');
+     }
+
+
+    window.addEventListener('resize', () => {
+        centerItem(currentIndex);
+    });
+}
+
+document.addEventListener('keydown', function(e) {
+    const blogs = document.querySelectorAll('.blog-container'); 
+    
+    if (e.key === 'ArrowLeft') {
+        const prevButton = document.querySelector('.prev-button');
+        prevButton.click(); 
+    }
+
+    
+    if (e.key === 'ArrowRight') {
+        const nextButton = document.querySelector('.next-button');
+        nextButton.click(); 
+    }
+
+});
